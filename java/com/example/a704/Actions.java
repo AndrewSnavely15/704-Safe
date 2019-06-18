b66b3625754f4938739e498786fae3f8caffd770
AndrewSnavely15 <32400601+AndrewSnavely15@users.noreply.github.com>
2019-06-18 12:55:39 -0700

    Add files via upload

---
+++ java/com/example/a704/Actions.java
@@ -0,0 +1,16 @@
+package com.example.a704;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Actions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actions_home);
+    }
+
+}
